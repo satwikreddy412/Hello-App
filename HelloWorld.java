cff522472cd7a4a75a583b6e272fbd20d3108c18
satwikreddy412 <satwiksabella0412@gmail.com>
2026-04-02 21:32:19 +0530

    UC7: Use String.join for multiple names with default greeting

--- HelloWorld.java
+++ HelloWorld.java
@@ -4,16 +4,8 @@ public class HelloWorld {
         if (args.length == 0) {
             System.out.println("Hello, World!");
         } else {
-            String result = "";
-
-            for (String name : args) {
-                result += name + ", ";
-            }
-
-            // Remove last ", "
-            result = result.substring(0, result.length() - 2);
-
-            System.out.println("Hello " + result);
+            String names = String.join(", ", args);
+            System.out.println("Hello " + names);
         }
     }
 }
