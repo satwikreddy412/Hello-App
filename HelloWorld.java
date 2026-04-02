b864fbcfac4984d712b0b71d52f403677692ef62
satwikreddy412 <satwiksabella0412@gmail.com>
2026-04-02 21:25:05 +0530

    UC5: Use enhanced for-loop for multiple names with default greeting

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,14 +2,16 @@ public class HelloWorld {
     public static void main(String[] args) {
 
         if (args.length == 0) {
-            System.out.println("Hello World");
+            System.out.println("Hello, World!");
         } else {
             System.out.print("Hello ");
 
-            for (int i = 0; i < args.length; i++) {
-                System.out.print(args[i]);
+            int count = 0;
+            for (String name : args) {
+                System.out.print(name);
+                count++;
 
-                if (i < args.length - 1) {
+                if (count < args.length) {
                     System.out.print(", ");
                 }
             }
