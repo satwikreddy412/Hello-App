6d1497f956213024fc1a03c357e3d155692d0f33
satwikreddy412 <satwiksabella0412@gmail.com>
2026-04-02 21:21:42 +0530

    UC4: Display Hello with multiple names or default World

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,11 +1,18 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        String name = "World";  // default value
 
-        if (args.length > 0) {
-            name = args[0];
+        if (args.length == 0) {
+            System.out.println("Hello World");
+        } else {
+            System.out.print("Hello ");
+
+            for (int i = 0; i < args.length; i++) {
+                System.out.print(args[i]);
+
+                if (i < args.length - 1) {
+                    System.out.print(", ");
+                }
+            }
         }
-
-        System.out.println("Hello " + name);
     }
 }
